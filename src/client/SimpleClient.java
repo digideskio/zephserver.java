ec94bbb58a967e9b8b89cd5de4bb676fa45adc51
Tom Rees <zephod@gmail.com>
2009-06-07 23:09:26 +0100

    Created eclipse project files.

--- src/client/SimpleClient.java
+++ src/client/SimpleClient.java
@@ -1,11 +1,8 @@
 package client;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 
 import org.apache.commons.logging.Log;
